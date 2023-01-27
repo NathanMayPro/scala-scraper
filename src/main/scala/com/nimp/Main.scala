07631466f728e296121112ced85d0a445c99ee48
NathanMayPro <nathan.may.pro@gmail.com>
2023-01-27 16:53:10 +0100

    simple api to retrieve data

--- src/main/scala/com/nimp/Main.scala
+++ src/main/scala/com/nimp/Main.scala
@@ -1,19 +1,37 @@
-package com.nimp
+
+import java.io._
+import java.net._
+import scala.io._
 
 object Main {
+  def main(args: Array[String]) = {
 
-    // retriev arguements from command line and print them
-
-    def fancyStr(string: String) = fansi.Color.Red(string)
-    def main(args: Array[String]): Unit = {
-        // to print all arguments
-        if (args.length > 0) {
-            println("Arguments:")
-            args.foreach(println)
-        } else {
-            println(fancyStr("You neeed to pass arguments to this program!"))
-        }
-        // to print a fancy string
-        
+    // check if the user has passed the key
+    if (args.length == 0) {
+        println("You need to pass the key as an argument!")
+        // exit the program
+        System.exit(1)
     }
+    val key = args(0)
+
+    // set all the parameters
+    val baseUrl = "http://api.weatherapi.com"
+    val serviuce = "/v1/current.json"
+    val keyword = "Paris"
+    // construct the final url
+    val url = baseUrl + serviuce + "?key=" + key + "&q=" + keyword
+
+    // create a connection
+    val conn = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
+    // set the request method
+    conn.setRequestMethod("GET")
+    // send data
+    val inputStream = conn.getInputStream
+    // read the response and transform it to a string
+    val content = Source.fromInputStream(inputStream).mkString
+    println(content)
+
+    // close the connection
+    inputStream.close()
+  }
 }
